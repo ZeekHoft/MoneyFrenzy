f0bfe86bfd206a400bfb9f84c970012da8048b89
Nero Hoya <dltposiedon@gmail.com>
2024-10-21 00:41:43 +0800

    speed over time for mobs needs fixing

--- mob.gd
+++ mob.gd
@@ -4,14 +4,25 @@ var health = 1
 @onready var business_man = get_node("/root/Game/businessMan")
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-
+var CycleCount: int = 0
+var speed: int = 1
 
 
 func _physics_process(delta):
+
 	var mob_direction = global_position.direction_to(business_man.global_position)
-	velocity = mob_direction * 100.0
+	velocity = mob_direction * speed
 	animated_sprite_2d.play("running")
 	move_and_slide()
+	
+	
+	#speed over time need fix
+	CycleCount += 1
+	for i in CycleCount:
+		if i % 1000 == 0:
+			speed += 1
+	print(speed)
+	
 	
 	var player_direction = business_man.get_player_direction()
 	#global_position.x is the mob then the other one is the x position of the player
@@ -30,13 +41,5 @@ func take_damage():
 		get_parent().add_child(loanded) #instead of being a child we're making it a sibling
 		#because queue free deletes the node so no loanded animation
 		loanded.global_position = global_position
-
-		
-	
-	
-	
-
-
-
 
 
