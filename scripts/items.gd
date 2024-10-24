4ab319bf28e92b80a64564f8a9e0fb926d21e94e
Nero Hoya <dltposiedon@gmail.com>
2024-10-24 13:21:00 +0800

    added sfx for pickup need a bit of fixing though

--- scripts/items.gd
+++ scripts/items.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 @onready var income = get_node("/root/Game/score/game_rev")
 @onready var bar_revenue = get_node("/root/Game/score/revenue_bar/rev_bar")
+@onready var pickup = $pickup
 
 
 
@@ -19,6 +20,7 @@ func _ready():
 var heal = 10.0
 
 func add_health():
+	pickup.play()
 	GlobalVar.business_reputation_health = min(GlobalVar.business_reputation_health + heal, 500.0)
 	bar_revenue.value = GlobalVar.business_reputation_health
 	
@@ -26,14 +28,20 @@ func add_health():
 
 	
 func _on_body_entered(_body):
-	
 	if item_type == 0:
 		income.add_rev()
+		pickup.play()
+		
 	if item_type == 1:
 		income.regain_revenue()
+		pickup.play()
+		
+		
 	if item_type == 2:
 		add_health()
-		print(bar_revenue.value)
+		
+		
+		#print(bar_revenue.value)
 	queue_free()
 	
 
